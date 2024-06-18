be23f544d2fbfa21352b7c98041b284b2693322d
tonible14012002 <namanh14.work@gmail.com>
2024-06-18 22:10:46 +0700

    ci(all): setup commitlint - husky

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang='en' suppressHydrationWarning>
       <body className={cn(inter.className)}>
+        asodij
         <AppProviders>{children}</AppProviders>
       </body>
     </html>
