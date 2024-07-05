c4031f8684b3023e8fbc38bdfeb8ac65f4ce490e
tonible14012002 <namanh14.work@gmail.com>
2024-07-05 10:44:10 +0700

    chore(all): remove dump

--- src/hooks/auth/useAuthEmailStepOne.tsx
+++ src/hooks/auth/useAuthEmailStepOne.tsx
@@ -3,10 +3,6 @@ import { useMutation } from '@tanstack/react-query'
 
 export const AUTHEN_EMAIL_STEP_ONE_KEY = 'v1/auth-services/email-step-one'
 
-export interface MutationAuthenEmailStepOneArgs {
-  email: string
-}
-
 export const useAuthenEmailStepOne = () => {
   return useMutation({
     mutationKey: [AUTHEN_EMAIL_STEP_ONE_KEY],
