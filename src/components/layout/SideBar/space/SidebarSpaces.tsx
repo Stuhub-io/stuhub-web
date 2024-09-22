7616e4e509d5dbd28ccdbbaeb705d4871b64a369
tonible14012002 <namanh14.work@gmail.com>
2024-09-22 15:31:40 +0700

    fix(sidebar): skeleton

--- src/components/layout/SideBar/space/SidebarSpaces.tsx
+++ src/components/layout/SideBar/space/SidebarSpaces.tsx
@@ -4,6 +4,7 @@ import { SidebarIconButton } from '../SidebarIconbutton'
 import { SidebarItem } from '../SidebarItem'
 import { useSidebar } from '@/components/providers/sidebar'
 import { SpaceItem } from './SidebarSpaceItem'
+import { SidebarItemSkeleton } from '../SidebarItemSkeleton'
 
 export const SidebarSpaces = () => {
   const { publicSpaces, privateSpace } = useSidebar()
@@ -26,9 +27,27 @@ export const SidebarSpaces = () => {
           {publicSpaces?.map((space) => {
             return <SpaceItem space={space} key={space.id} />
           })}
+          {!publicSpaces && (
+            <>
+              <SidebarItemSkeleton className="w-14" />
+              <SidebarItemSkeleton hasIcon />
+              {[1, 2, 3, 4].map((i) => (
+                <SidebarItemSkeleton hasIcon delay={300 + 100 * i} key={i} />
+              ))}
+            </>
+          )}
         </CollapsibleContent>
       </Collapsible>
       {privateSpace && <SpaceItem space={privateSpace} />}
+      {!privateSpace && (
+        <>
+          <SidebarItemSkeleton className="w-14" />
+          <SidebarItemSkeleton hasIcon />
+          {[1, 2, 3, 4].map((i) => (
+            <SidebarItemSkeleton hasIcon delay={300 + 200 * i} key={i} />
+          ))}
+        </>
+      )}
     </div>
   )
 }
