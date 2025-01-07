a3227ab17e4898a4f9399b11962913e8e2f786bc
khoatruong19 <khoa-truong@cybozu.vn>
2025-01-07 16:16:25 +0700

    feat(page): recent pages list

--- src/app/(app)/[orgSlug]/recent/page.tsx
+++ src/app/(app)/[orgSlug]/recent/page.tsx
@@ -1,11 +1,48 @@
 'use client'
 
+import Typography from '@/components/common/Typography'
+import { PageListView } from '@/components/page/PageListView/PageListView'
+import { EmptyListPlaceholder } from '@/components/page/asset/EmpyListPlaceholder'
+import { FolderViewToolbar } from '@/components/page/page_view/page_viewers/PageFolderViewer/Toolbar'
 import { useFetchPageAccessLogs } from '@/mutation/querier/page-access-log/useFetchPageAccessLogs'
+import { Selection } from '@nextui-org/react'
+import { useState } from 'react'
 
 export default function Page() {
-  const { data } = useFetchPageAccessLogs({
+  const { data: { data: logs } = {}, refetch } = useFetchPageAccessLogs({
     allowFetch: true,
   })
 
-  return <>Hello {JSON.stringify(data)}</>
+  const [selectedPagePkIDs, setSelectedPagePkIDs] = useState<number[]>([])
+
+  const [typeFilter, setTypeFilter] = useState<Selection>('all')
+
+  const filesAndDocs = logs?.map((log) => log.page)
+
+  return (
+    <>
+      <div className="md:px-4">
+        <div className="my-8 grid grid-cols-1 gap-2">
+          <Typography level="h4">Recent</Typography>
+
+          <FolderViewToolbar
+            isViewOnly
+            typeFilter={typeFilter}
+            onCreateFolderClick={() => {}}
+            onTypeFilterChange={setTypeFilter}
+            onUploadClick={() => {}}
+          />
+        </div>
+
+        <PageListView
+          items={filesAndDocs}
+          onItemMutateSuccess={refetch}
+          onItemDoubleClick={() => {}}
+          selectedItemPkIDs={selectedPagePkIDs}
+          onSelectedPkIDsChanged={setSelectedPagePkIDs}
+          emptyState={<EmptyListPlaceholder onClick={() => {}} />}
+        />
+      </div>
+    </>
+  )
 }
